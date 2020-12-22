71f94247dfe928a4b18a490f39283bc77dee7aa8
yourgurunet <info@yourgurunet.com>
2020-12-22 08:44:19 +0200

    Getting details about specific product with matching id

--- src/pages/ProductDetails.js
+++ src/pages/ProductDetails.js
@@ -1,11 +1,28 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { useParams } from 'react-router-dom';
-
+import { ProductContext } from '../context/products';
+import { useHistory } from 'react-router-dom';
+import Loading from '../components/Loading';
 const SingleProduct = () => {
   // Id for exact page
   //console.log(useParams());
   const { id } = useParams();
-  return <div>Single Product {id}</div>;
+  const { products } = useContext(ProductContext);
+  //check if id mach
+  const product = products.find((item) => item.id === parseInt(id));
+  const {
+    image: { url },
+    title,
+    description,
+    price,
+  } = product;
+  return products.length === 0 ? (
+    <Loading />
+  ) : (
+    <React.Fragment>
+      <h1>{title}</h1>
+    </React.Fragment>
+  );
 };
 
 export default SingleProduct;
