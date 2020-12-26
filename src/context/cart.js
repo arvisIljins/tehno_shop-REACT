4dc99e6f22bb5197cd0952d4f6e60193f82e90e3
yourgurunet <info@yourgurunet.com>
2020-12-26 12:32:05 +0200

    useEffeect to get total amount and total proce

--- src/context/cart.js
+++ src/context/cart.js
@@ -1,4 +1,4 @@
-import React, { createContext } from 'react';
+import React, { createContext, useEffect } from 'react';
 import TestCart from '../utils/TestCart';
 
 const CartContext = createContext();
@@ -7,8 +7,25 @@ const CartProvider = ({ children }) => {
   const [cart, setCart] = React.useState(TestCart);
   const [total, setTotal] = React.useState(0);
   const [cartItem, setCartItem] = React.useState(0);
+
+  useEffect(() => {
+    //set total amount
+    let newCartItem = cart.reduce((total, cartItem) => {
+      return (total += cartItem.amount);
+    }, 0);
+    setCartItem(newCartItem);
+    //set total
+    let newTotal = cart.reduce((total, cartItem) => {
+      return (total += cartItem.amount * cartItem.price);
+    }, 0);
+    newTotal = parseFloat(newTotal.toFixed(2));
+    setTotal(newTotal);
+  }, [cart]);
+  const deleteItem = (id) => {
+    console.log('delete');
+  };
   return (
-    <CartContext.Provider value={{ cart, total, cartItem }}>
+    <CartContext.Provider value={{ cart, total, cartItem, deleteItem }}>
       {children}
     </CartContext.Provider>
   );
