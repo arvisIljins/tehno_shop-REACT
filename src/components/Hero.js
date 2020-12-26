37a85b8197084d6b39ef863b87dc95ff62db3a30
yourgurunet <info@yourgurunet.com>
2020-12-26 08:46:34 +0200

    Hero fixes

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,22 +1,18 @@
 import styled from 'styled-components';
 
-
 const Hero = styled.section`
-width: 100vw;
-height: 60vh;
-background: linear-gradient(rgba(0, 0, 0, 0.8), rgba(0, 0, 0, 0.8)),
+  max-width: 155rem;
+  height: 60vh;
+  margin: 0 auto;
+  background: linear-gradient(rgba(0, 0, 0, 0.8), rgba(0, 0, 0, 0.8)),
     url(${(img) => img.img}) center/cover no-repeat;
-display:flex;
-align-items: center;
+  display: flex;
+  align-items: center;
 
-
-
-
-:hover {
+  :hover {
     background: linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)),
-    url(${(img) => img.img}) center/cover no-repeat;
-}
-
+      url(${(img) => img.img}) center/cover no-repeat;
+  }
 `;
 
-export default Hero
+export default Hero;
